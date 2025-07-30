89117618c7413dde1b51f813c494b58fde9431d9
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-30 10:12:10 +0600

    Create 19. Remove Nth Node From End of List1.cpp

---
+++ 19. Remove Nth Node From End of List1.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // base case
+        if (head == null) {
+            return null;
+        }
+
+        // size
+        int size = 0;
+        ListNode currNode = head;
+        while (currNode != null) {
+            size++;
+            currNode = currNode.next;
+        }
+
+        // another base case
+        if (size == n) {
+            return head.next;
+        }
+
+        int previousNodeIndex = size - n;
+        ListNode prev = head;
+        int i = 1;
+        while (i < previousNodeIndex) {
+            prev = prev.next;
+            i++;
+        }
+
+        // break the connection
+        prev.next = prev.next.next;
+
+        return head;
+    }
+}
