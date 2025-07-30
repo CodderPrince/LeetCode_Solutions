92a99aca770c1d1adbc7c71faa5784bb0024a9ad
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-30 10:06:05 +0600

    Create 51. N-Queens.py

---
+++ 51. N-Queens.py
@@ -0,0 +1,40 @@
+class Solution:
+    def __init__(self):
+        self.result = []
+
+    def solve(self, board, row, cols, diags, antiDiags):
+        if row == len(board):
+            self.result.append(["".join(row) for row in board])
+            return
+        
+        for col in range(len(board)):
+            diag_id = row - col
+            anti_diag_id = row + col
+            
+            if col in cols or diag_id in diags or anti_diag_id in antiDiags:
+                continue
+            
+            cols.add(col)
+            diags.add(diag_id)
+            antiDiags.add(anti_diag_id)
+            board[row][col] = 'Q'
+            
+            self.solve(board, row + 1, cols, diags, antiDiags)
+
+            cols.remove(col)
+            diags.remove(diag_id)
+            antiDiags.remove(anti_diag_id)
+            board[row][col] = '.'
+
+    def solveNQueens(self, n):
+        if n == 0:
+            return []
+        board = [['.' for _ in range(n)] for _ in range(n)] 
+        
+        start_row = 0
+        cols = set()
+        diags = set()
+        antiDiags = set()
+        self.solve(board, start_row, cols, diags, antiDiags)
+        
+        return self.result
