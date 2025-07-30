c4813266085b4ea5000d9a3f68b558e14655ad42
Md. An Nahian Prince <annahian44@gmail.com>
2025-07-30 09:58:49 +0600

    Create 1092. Shortest Common Supersequence2.cpp

---
+++ 1092. Shortest Common Supersequence2.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int dp[1001][1001]; // Global array
+    
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.length();
+        int m = str2.length();
+        
+        // Initializing dp array to -1
+        memset(dp, -1, sizeof(dp));
+        
+        // Filling the dp table using recursion
+        int lcs_length = lcs(str1, str2, n, m);
+        
+        string scs = "";
+        int i = n, j = m;
+        while (i > 0 && j > 0) {
+            if (str1[i-1] == str2[j-1]) {
+                scs += str1[i-1];
+                i--;
+                j--;
+            } else if (dp[i][j-1] > dp[i-1][j]) {
+                scs += str2[j-1];
+                j--;
+            } else {
+                scs += str1[i-1];
+                i--;
+            }
+        }
+        while (i > 0) {
+            scs += str1[i-1];
+            i--;
+        }
+        while (j > 0) {
+            scs += str2[j-1];
+            j--;
+        }
+        reverse(scs.begin(), scs.end());
+        return scs;
+    }
+    
+    int lcs(string& str1, string& str2, int n, int m) {
+        if (n == 0 || m == 0)
+            return 0;
+        
+        if (dp[n][m] != -1)
+            return dp[n][m];
+        
+        if (str1[n-1] == str2[m-1])
+            return dp[n][m] = 1 + lcs(str1, str2, n-1, m-1);
+        else
+            return dp[n][m] = max(lcs(str1, str2, n-1, m), lcs(str1, str2, n, m-1));
+    }
+};
